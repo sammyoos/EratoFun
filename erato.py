5e164cfaf234cf16a1953ff743f0fdee081a6eea
Samuel Oosterhuis <coder@sammyo.com>
2015-01-04 21:12:40 -0500

    much cleaner

--- erato.py
+++ erato.py
@@ -24,24 +24,17 @@ def printDocHeader():
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
 		<title>EratoFun Data Table</title>
-
 		<style>
 			body { font-family: sans-serif; }
 			table { border-collapse: collapse; }
 			td { border-right-style: solid; border-left-style: solid; padding: 2px; }
 			td.breakPat { background: LightGrey; }
 		</style>
-
 	</head>
-
 <body>
-
 <h1>Sieve of Eratosthenes</h1>
-
-<table id="EratoData">
-	<tbody>
+<table id="EratoData"><tbody>
 """ )
 
 
@@ -51,13 +44,38 @@ def printDocFooter():
 </html>
 """ )
 
+def processNewPrime( prime, erato, indent, searchDomain, maxColumnCount ):
+
+	# nothing left to display
+	if( indent > maxColumnCount ):
+		return( False )
+
+	print( '<tr><th>%s</th>' % prime, end='' )
+	print( '<td colspan="%d">&nbsp;</td>' % indent, end='' )
+
+	gap = 1
+	column_count = indent
+	first = int(( prime * prime ) / 2)
+	erato[first]=prime
+
+	for index in xrange( first+prime, searchDomain+1, prime ):
+		if( erato[index] != 0 ):
+			gap += 1
+		else:
+			if( column_count <= maxColumnCount ):
+				column_count += 1
+				print( '<td class=c%03d>%s</td>' % ( gap, gap ), end='' )
+			erato[index]=prime
+			gap = 1
+
+	print( '</tr>' )
+	return( True )
 
 
 def startSieve( maxPrimeCandidate = 3000, maxColumnCount = 100 ):
-	max_domain = maxPrimeCandidate*maxPrimeCandidate
-	max_half_domain = max_domain / 2
+	maxHalfDomain = maxPrimeCandidate*maxPrimeCandidate / 2
 
-	erato = [ 0 for i in range( max_half_domain + 1 ) ]
+	sieve = [ 0 for i in range( maxHalfDomain + 1 ) ]
 
 
 	# index will be related to candidate as follows
@@ -65,47 +83,22 @@ def startSieve( maxPrimeCandidate = 3000, maxColumnCount = 100 ):
 	# and only odd numbers will be evaluated
 	# the number 2 is prime, but will not be considered
 
-	#column_count = 0
-	indent = 0
-	initial_index = 0
-	candidate = 3
-
+	rowIndent = 0
+	initialIndex = 0
 
 	printDocHeader()
-	while( candidate < maxPrimeCandidate ):
-		initial_index = initial_index + 1
-		if( erato[initial_index] == 0 ):
-			print( '<tr><th>%s</th>' % candidate, end='' )
-
-			column_count = indent
-			indent = indent + 1
-			print( '<td colspan="%d">&nbsp;</td>' % indent, end='' )
-
-			increment = 1
-			index = ( candidate * candidate ) / 2
-			erato[index]=candidate
-			index = index + candidate
-
-			while( index <= max_half_domain ):
-				if( erato[index] != 0 ):
-					increment = increment + 1
-				else:
-					if( column_count <= maxColumnCount ):
-						column_count = column_count + 1
-						print( '<td class=c%03d>%s</td>' % ( increment, increment ), end='' )
-					erato[index]=candidate
-					increment = 1
-
-				index = index + candidate
-			print( '</tr>' )
-			#pp.pprint( erato )
-		candidate = candidate + 2
+	for candidate in xrange( 3, maxPrimeCandidate, 2 ):
+		initialIndex += 1
+		if( sieve[initialIndex] == 0 ):
+			rowIndent += 1
+			if( not processNewPrime( candidate, sieve, rowIndent, maxHalfDomain, maxColumnCount )):
+				break
 		print( '' )
 
 	printDocFooter()
                     
 
-startSieve( maxPrimeCandidate = 3000, maxColumnCount = 100 )
+startSieve( maxPrimeCandidate = 10000, maxColumnCount = 300 )
 
 
 # vim: set noexpandtab ts=4 sw=4 foldmethod=marker:
