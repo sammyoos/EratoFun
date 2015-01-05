63edb846784d6b383be8a12b9491095d73e57b99
Samuel Oosterhuis <coder@sammyo.com>
2015-01-04 23:07:58 -0500

    final bit for the holidays..

--- erato.py
+++ erato.py
@@ -11,11 +11,7 @@ to execute:
 to document:
 > pydoc -w ./erato.py
 """
-
 from __future__ import print_function
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
 
 def printDocHeader():
 	print( """
@@ -29,7 +25,7 @@ def printDocHeader():
 			body { font-family: sans-serif; }
 			table { border-collapse: collapse; }
 			td { border-right-style: solid; border-left-style: solid; padding: 2px; }
-			td.breakPat { background: LightGrey; }
+			td.breakPat { background: DarkKhaki; }
 		</style>
 	</head>
 <body>
@@ -44,11 +40,13 @@ def printDocFooter():
 </html>
 """ )
 
-def processNewPrime( prime, erato, indent, searchDomain, maxColumnCount ):
+def processNewPrime( prime, erato, prev, indent ):
+	global maxColumnCount
+	global maxHalfDomain
 
 	# nothing left to display
 	if( indent > maxColumnCount ):
-		return( False )
+		return( None )
 
 	print( '<tr><th>%s</th>' % prime, end='' )
 	print( '<td colspan="%d">&nbsp;</td>' % indent, end='' )
@@ -58,47 +56,65 @@ def processNewPrime( prime, erato, indent, searchDomain, maxColumnCount ):
 	first = int(( prime * prime ) / 2)
 	erato[first]=prime
 
-	for index in xrange( first+prime, searchDomain+1, prime ):
+	curr = []
+	prevIdx = 1
+	patternBroken = True if( prev is None ) else False
+
+	for index in xrange( first+prime, maxHalfDomain+1, prime ):
 		if( erato[index] != 0 ):
 			gap += 1
 		else:
 			if( column_count <= maxColumnCount ):
 				column_count += 1
-				print( '<td class=c%03d>%s</td>' % ( gap, gap ), end='' )
+				curr.append( gap )
+
+				if( not patternBroken and gap != prev[prevIdx] ):
+					patternBroken = True
+				else:
+					prevIdx += 1
+
+				if( patternBroken ):
+					print( '<td class=breakPat>%s</td>' % gap, end='' )
+				else:
+					print( '<td>%s</td>' % gap, end='' )
+
 			erato[index]=prime
 			gap = 1
 
 	print( '</tr>' )
-	return( True )
+	#print( '!--', curr, '--!' )
+	return( curr )
 
 
-def startSieve( maxPrimeCandidate = 3000, maxColumnCount = 100 ):
-	maxHalfDomain = maxPrimeCandidate*maxPrimeCandidate / 2
+# index will be related to candidate as follows
+# index = (int)(candidate/2)
+# and only odd numbers will be evaluated
+# the number 2 is prime, but will not be considered
+def startSieve():
+	global maxPrimeCandidate
+	global maxHalfDomain
 
 	sieve = [ 0 for i in range( maxHalfDomain + 1 ) ]
-
-
-	# index will be related to candidate as follows
-	# index = (int)(candidate/2)
-	# and only odd numbers will be evaluated
-	# the number 2 is prime, but will not be considered
-
+	prev = None
 	rowIndent = 0
 	initialIndex = 0
-
 	printDocHeader()
+
 	for candidate in xrange( 3, maxPrimeCandidate, 2 ):
 		initialIndex += 1
 		if( sieve[initialIndex] == 0 ):
 			rowIndent += 1
-			if( not processNewPrime( candidate, sieve, rowIndent, maxHalfDomain, maxColumnCount )):
+			prev = processNewPrime( candidate, sieve, prev, rowIndent )
+			if( prev is None ):
 				break
 		print( '' )
 
 	printDocFooter()
                     
 
-startSieve( maxPrimeCandidate = 10000, maxColumnCount = 300 )
+maxPrimeCandidate = 10000
+maxColumnCount = 500
+maxHalfDomain = maxPrimeCandidate*maxPrimeCandidate / 2
 
-
+startSieve()
 # vim: set noexpandtab ts=4 sw=4 foldmethod=marker:
